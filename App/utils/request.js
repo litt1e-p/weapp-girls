7791273e2acdde251646f072ae5bf0528dfc121a
litt1e-p <free9fw@gmail.com>
2016-11-23 17:50:35 +0800

    fix bug of Promise import in MINA v0.11.112300

--- App/utils/request.js
+++ App/utils/request.js
@@ -1,14 +1,15 @@
+import Promise from 'es6-promise.min';
 module.exports = (options) => {
     return new Promise((resolve, reject) => {
         options = Object.assign(options, {
             success(result) {
-                if (result.statusCode === 200) {
-                    resolve(result.data);
-                } else {
-                    reject(result);
-                }
-            },
-            fail: reject,
+                    if (result.statusCode === 200) {
+                        resolve(result.data);
+                    } else {
+                        reject(result);
+                    }
+                },
+                fail: reject,
         });
         wx.request(options);
     });
